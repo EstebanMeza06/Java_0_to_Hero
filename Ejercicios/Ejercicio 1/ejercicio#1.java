e735514cded4f5043cf21c54a1ec46ad54ffb298
Esteban Meza <estebansklestebanskl@gmail.com>
2025-05-04 11:25:59 -0500

    Create ejercicio#1.java

---
+++ Ejercicios/Ejercicio 1/ejercicio#1.java
@@ -0,0 +1,6 @@
+public class Ejercicio1{
+    public static void main(){
+      system.out.print(" Perfil Github: Esteban Meza06");
+      system.out.print("https://www.linkedin.com/in/esteban-josé-meza-teran-a20610119/");
+    }
+}
