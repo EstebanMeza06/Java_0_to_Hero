f34a22ab98c8c78bb3476eb9c702cc8d9a5ea853
Esteban Meza <estebansklestebanskl@gmail.com>
2025-05-04 12:08:44 -0500

    Create Ejercicio2.java

---
+++ Ejercicios/Ejercicio 2/Ejercicio2.java
@@ -0,0 +1,18 @@
+//Declara tres variables de diferentes tipos, asígnales valores y luego imprímelas.
+public class ejercicio2{
+  public static void main(){
+      String Nombre= "Esteban";
+        var  Genero= "Masculino";
+        int Edad=22;
+          System.out.println("El cliente es " + Nombre + " tiene " + Edad + " años y es de genero " + Genero);
+//Crea un programa que declare dos variables enteras, asigne valores a estas variables y luego imprima la suma de esos valores.
+    int Suma1= 20;
+    int Suma2=30;
+    int Result= Suma1+ Suma2;
+      System.out.println(Result);
+//Crea un programa que declare variables para almacenar los costos de diferentes compras realizadas en un día, sume estos costos y luego imprima el total.
+    int Arroz=20000;
+    int Pollo=30000;
+    int Huevos=15000;
+    int ComprasRealizadas= Arroz + Pollo + Huevos;
+      System.out.print("compras realizadas en un día fue de: "ComprasRealizadas);
