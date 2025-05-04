0c33edc4ab8212487ccd62a9648e1b6bbc5251fc
Esteban Meza <estebansklestebanskl@gmail.com>
2025-05-04 11:36:08 -0500

    Create Ejercicio1.java

---
+++ Ejercicios/Ejercicio 1/Ejercicio1.java
@@ -0,0 +1,7 @@
+//"Para este primer ejercicio, requieres hacer un programa que imprima por consola tu nombre de usuario y el link de tu perfil de GitHub."
+  public class ejercicio1{
+    public static void main(){
+      system.out.print("Nombre de usuario: Esteban Meza06");
+      system.out.print("link: https://github.com/EstebanMeza06");
+    }
+  }
